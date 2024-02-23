8ed374a002fe0341544bcf053e1b158b6a4ec2f4
Abhishek65a <atupsundar3@gmail.com>
2024-02-23 11:46:18 +0530

    pass the message

--- Pass_the_message/script.js
+++ Pass_the_message/script.js
@@ -1,10 +1,9 @@
-const messageInput=document.getElementById("message-input")
-messageInput.addEventListener("keydown",function(event){
-    if(event.key == "Enter")
-    getMessage()
-})
+const messageInput = document.getElementById("message-input");
+messageInput.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") getMessage();
+});
 
-function getMessage(){
-    document.getElementById("message-output").innerHTML = messageInput.value;
-    messageInput.value = "";
+function getMessage() {
+  document.getElementById("message-output").innerHTML = messageInput.value;
+  messageInput.value = "";
 }
